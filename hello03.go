cd276708c1f76529a1720de78df5411d40c3af6f
HiBo001 <770877935@qq.com>
2021-07-30 06:12:48 -0700

    hello03.go

--- hello03.go
+++ hello03.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 type Vertex struct {
 	X int
@@ -17,6 +20,10 @@ var (
 	v3 = Vertex{}
 	p = &Vertex{1,2}
 )
+
+func printSlice(s[]int){
+	fmt.Println("len=%d cap=%d %v ", len(s), cap(s), s)
+}
 
 func main() {
 	i, j := 42, 2700
@@ -76,4 +83,38 @@ func main() {
 		{5,true},
 	}
 	fmt.Println(s1)
+
+	s2 := []int{2, 3, 5, 7, 11, 13}
+	s2 = s2[1:4]
+	fmt.Println(s2)
+	fmt.Println(s2[:2])
+	fmt.Println(s2[0:])
+
+	s3 := []int{2,3,5,7,11,13}
+	printSlice(s3)
+
+	s3 = s3[:0]
+	printSlice(s3)
+
+	s3 = s3[:4]
+	printSlice(s3)
+
+	s3 = s3[2:]
+	printSlice(s3)
+
+	var s4[]int
+	fmt.Println(s4, len(s4), cap(s4))
+	if s4 == nil{
+		fmt.Println("nil")
+	}
+
+	board := [][]string{
+		[]string{"1","2","3"},
+		[]string{"4","5","6"},
+		[]string{"7","8","9"},
+	}
+
+	for i:=0; i<len(board); i++ {
+		fmt.Println(strings.Join(board[i], " "))
+	}
 }
