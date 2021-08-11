972cb4c3dc9d7596211e6ee37d32a86c89b7a9f1
HiBo001 <770877935@qq.com>
2021-08-11 15:18:26 +0800

    hello05

--- hello05.go
+++ hello05.go
@@ -12,7 +12,48 @@ func say(s string){
 	}
 }
 
+func sum(s[]int, c chan int) {
+	sum := 0
+	for _, v:= range s {
+		sum += v
+	}
+	c <- sum
+}
+
+func fibonacci(n int, c chan int) {
+	x, y := 0, 1
+	for i:= 0; i < n; i++ {
+		c <- x
+		x, y = y, x+y
+	}
+	close(c)
+}
+
 func main() {
 	go say("world")
 	say("hello")
+
+	s := []int{7, 2, 8, -9, 4, 0}
+	c := make(chan int)
+
+	go sum(s[:len(s)/2], c)
+	go sum(s[len(s)/2:], c)
+
+	x, y := <-c, <-c
+	fmt.Println(x, y, x+y)
+
+	c1 := make(chan int, 2)
+	c1 <- 1
+	c1 <- 2
+	fmt.Println(<-c1, <-c1)
+
+	c2 := make(chan int, 10)
+
+	fmt.Println(cap(c2))
+	
+	go fibonacci(cap(c2), c2)
+
+	for i := range c2 {
+		fmt.Println(i)
+	}
 }
